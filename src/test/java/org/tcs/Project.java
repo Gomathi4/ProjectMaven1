c94b554f8e26e2297cfe4b8710f89543276a82be
Gomathi4 <selvigoms74@gmail.com>
2022-08-24 12:00:25 +0530

    Added details of a person

---
+++ src/test/java/org/tcs/Project.java
@@ -0,0 +1,14 @@
+package org.tcs;
+
+public class Project {
+
+	public void name() {
+		System.out.println("Name");
+	}
+	public void address() {
+		System.out.println("Chennai");
+	}
+	public void phno() {
+		System.out.println("6381690865");
+	}
+}
