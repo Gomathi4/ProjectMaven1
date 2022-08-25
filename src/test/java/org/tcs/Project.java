278827d9d8bc59b739efdbc59759b7aa0b1a8702
Gomathi4 <selvigoms74@gmail.com>
2022-08-25 14:31:29 +0530

    newly added methods

--- src/test/java/org/tcs/Project.java
+++ src/test/java/org/tcs/Project.java
@@ -11,14 +11,23 @@ public class Project {
 	public void phno() {
 		System.out.println("6381690865");
 	}
+	public void india() {
+		System.out.println("india");
+	}
+	public void australia() {
+		System.out.println("australia");
+	}
+	public void canada() {
+		System.out.println("canada");
+	}
 	public void tamilnadu() {
 		System.out.println("tamilnadu");
 	}
 	public void kerala() {
 		System.out.println("kerala");
 	}
-	public void karnataga() {
-		System.out.println("karnataga");
+	public void goa() {
+		System.out.println("goa");
 	}
 
 }
